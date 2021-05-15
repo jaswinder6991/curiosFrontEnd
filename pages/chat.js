dc89155a29f21bdbb8a2922a27f2a30f53dbdf7c
Jaswinder Singh <jassification@192.168.1.3>
2021-05-15 23:50:57 +0530

    chat initial

---
+++ pages/chat.js
@@ -0,0 +1,20 @@
+import Layout from "../components/layout"
+import Head from 'next/head'
+import dynamic from 'next/dynamic'
+
+
+const chatbox = () => {
+  
+    return (
+    <>
+    <Layout>
+    <main>
+        
+      </main>
+      
+    </Layout>
+    </>
+)
+}
+
+export default chatbox
--- pages/test.js
+++ pages/test.js
@@ -1,52 +1,169 @@
-import { useUser } from '../lib/hooks'
-import Layout from '../components/layout'
-import dynamic from 'next/dynamic'
+// import Head from "next/head";
+// //import styles from "../styles/Home.module.css";
+// import { useState, useEffect } from 'react';
+// import Layout from "../components/layout";
 
-const DynamicComponentWithNoSSR = dynamic(
-  () => import('waitlistapi').then(module => module.Waitlist),
-  { ssr: false }
-)
+// export default function Home() {
+//   const [data, setData] = useState( { text:'' });
+//   const [query, setQuery] = useState();
+//   const [search, setSearch] = useState();
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [receivedMessages, setMessages] = useState([]);
 
-const Home = () => {
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       if (search) {
+//       setIsLoading(true);
+//       const res = await fetch(`/api/openai`, {
+//         body: JSON.stringify({
+//           name: search
+//         }),
+//         headers: {
+//           'Content-Type': 'application/json'
+//         },
+//         method: 'POST'
+//       })
+//       const data = await res.json();
+//       setData(data);
+//       setMessages(data.text)
+//       setIsLoading(false);
+//     }};
 
-const user = useUser()
+//     fetchData();
+//   }, [search]);
+//   return (
+//     <Layout>
+//    <div className="border-solid rounded-sm shadow-md">
+//       <div className="block bg-white rounded px-8 pt-6 pb-8 mb-4">
+        
+//       </div>
+//       {isLoading ? (
+//             <div>Typing ...</div>
+//          ) : (
+//            <>
+//            <span>
+//               {search}
+//            </span>
+//            <span>
+//            {data.text}
+//            </span>
+//            </>
+//            )}
+//       <input type="text" value={query} onChange={event => setQuery(event.target.value)}/>
+//         <button className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold pl-2 py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={() =>setSearch(query)}>
+//           Send
+//         </button>
 
-return (
+//         <form onSubmit={sendMessage}>
+//           <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
 
+//           <button type="submit" disabled={!formValue}>Send</button>
 
-<>
-    <div className="mx-80 mt-10 max-w-2xl px-4 sm:mt sm:px-6 md:mt lg:mt lg:px-8 xl:mt">
-    {/* </div><div className="mt-10 mx-auto max-w-7xl px-4 sm:mt sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28"> */}
-    <DynamicComponentWithNoSSR api_key="3G6AD9" waitlist_link="http://192.168.1.11:3000/"/>
-    <style jsx>{`
-      h1 {
-        color: #000000;
-      }
-      .container--waitlistapi {
-        margin: 0 auto; // centers the widget
-        background-color: #dad9d9;
-        max-width: 21rem;
-          margin: 0 auto;
-          padding: 2rem;
-          border: 0px solid #ccc;
-          border-radius: 4px;
-      }
-      .button--waitlistapi {
-        background-color: #8B5CF6;
-      }
-      .statusTextContainer--waitlistapi {
-        color: #000000;
-      }
-      .referralLinkField--text {
-        color: #000000;
-      }`}
-    </style>
+//         </form>  
+//     </div>
+//     </Layout>
+//   );
+// }
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+import uuid from "uuid";
+import Layout from "../components/layout";
+//import "./styles.css";
+
+const Messages = ({ messages }) => {
+  const messagesEndRef = useRef(null);
+  const scrollToBottom = () => {
+    messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+  useEffect(scrollToBottom, [messages]);
+
+  return (
+    <div className="messagesWrapper">
+      {messages.map((message) => (
+        <span key={message}>{message}</span>
+      ))}
+      <div ref={messagesEndRef} />
     </div>
-  </>
+  );
+};
 
+function App() {
+  const [messages, setMessages] = useState([]);
+  const [data, setData] = useState( { text:'' });
+  const [query, setQuery] = useState();
+  const [search, setSearch] = useState();
+  const [isLoading, setIsLoading] = useState(false);
 
-)
+  const addMessages = (e) => {
+    e.preventDefault();
+    setSearch(query);
+    setMessages((m) => [...m, query]);
+  };
 
+  const handleChange = (e) => {
+    setQuery(e.target.value);
+    //e.target.reset();
+  };
+
+  useEffect(() => {
+        const fetchData = async () => {
+          if (search) {
+          setIsLoading(true);
+          const res = await fetch(`/api/openai`, {
+            body: JSON.stringify({
+              name: search
+            }),
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            method: 'POST'
+          })
+          const data = await res.json();
+          setMessages((m) => [...m, data.text]);
+          setIsLoading(false);
+        }};
+    
+        fetchData();
+      }, [search]);
+
+  return (
+    <Layout>
+    <div className="App">
+    {/* <form onSubmit={addMessages}> */}
+    <form onSubmit={addMessages}>
+      <Messages messages={messages} />
+      {/* <input className="shadow appearance-none border rounded w-auto py-2 px-3 text-gray-700 leading-tight 
+    focus:outline-none focus:shadow-outline" type="text" value={query} onChange={event => setQuery(event.target.value)}/> */}
+    <input className="shadow appearance-none border rounded w-auto py-2 px-3 text-gray-700 leading-tight 
+    focus:outline-none focus:shadow-outline" type="text" value={query} onChange={handleChange}/>
+      {/* <button className="addButton"onClick={addMessages}> */}
+      <button type="submit">Send</button>
+      </form>
+    </div>
+    <style>{`
+    .App {
+      font-family: sans-serif;
+      text-align: center;
+    }
+    
+    .addButton {
+      border: 1px black solid;
+      font-size: 13px;
+      margin-top: 1rem;
+    }
+    
+    .messagesWrapper {
+      display: flex;
+      flex-direction: column;
+      border: 1px black solid;
+      width: auto;
+      height: 100px;
+      overflow: scroll;
+      margin-top: 1rem;
+    }
+    `}</style>
+    </Layout>
+  );
 }
 
-export default Home
+export default App
